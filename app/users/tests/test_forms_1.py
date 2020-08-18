a7ff587eab56cb9ea78ec6dc7dafe5bf1e41d878
adamjanas <a.janas@protonmail.com>
2020-08-18 19:38:04 +0200

    tests for users /views,forms,decorators

---
+++ app/users/tests/test_forms_1.py
@@ -0,0 +1,48 @@
+import os
+from django.test import TestCase
+from app.users.forms import UserAdminCreateForm
+from app.users.models import User
+from django.test.client import Client
+
+
+class FormTestCase(TestCase):
+
+    client = Client()
+
+    def setUp(self):
+
+        self.client = Client()
+        self.my_admin = User.objects.create_superuser('admin', 'password')
+
+    def test_admin_create_form_with_no_phone_number(self):
+        self.client.login(username='admin', password='password')
+
+        data = {
+            'username': 'testuser',
+            'email': 'test@test.com',
+            'phone_number': None,
+            'role': 1,
+            'password1': 'testpassword',
+            'password2': 'testpassword'
+        }
+
+        form = UserAdminCreateForm(data=data)
+
+        self.assertFalse(form.is_valid())
+
+    def test_admin_create_form_with_no_role(self):
+        self.client.login(username='admin', password='password')
+
+        data = {
+            'username': 'testuser',
+            'email': 'test@test.com',
+            'phone_number': 123123123,
+            'role': None,
+            'password1': 'testpassword',
+            'password2': 'testpassword'
+        }
+
+        form = UserAdminCreateForm(data=data)
+
+        self.assertFalse(form.is_valid())
+
