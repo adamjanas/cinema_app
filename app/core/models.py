34f68374ae6b145e9f104fae6ac69c16006f954a
adamjanas <a.janas@protonmail.com>
2020-08-24 11:45:40 +0200

    corrections in core

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from app.users.models import User
 
-# Create your models here.
+
+class CreatedAtAbstractModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class BaseAbstractModel(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    content = models.TextField()
+
+
+    def __str__(self):
+        return f"{self.name} - {self.author}"
+
+    class Meta:
+        abstract = True
