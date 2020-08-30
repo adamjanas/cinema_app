5decde91436a0837ce47cb4c681f984586978601
adamjanas <a.janas@protonmail.com>
2020-08-30 14:33:20 +0200

    format models.py using autopep8

--- app/clients/models.py
+++ app/clients/models.py
@@ -12,7 +12,8 @@ class Booking(CreatedAtAbstractModel):
     surname = models.CharField(max_length=50)
     ticket = models.ForeignKey(Price, on_delete=models.CASCADE)
     seat = models.ForeignKey(Seat, on_delete=models.CASCADE)
-    show = models.ForeignKey(Show, on_delete=models.CASCADE, related_name='shows')
+    show = models.ForeignKey(
+        Show, on_delete=models.CASCADE, related_name='shows')
 
     def __str__(self):
         return f"{self.user} reservation"
